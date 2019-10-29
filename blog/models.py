8d2dff59eb7e95c96274daee7a77694e66027695
Dalai Aguirre <49468436+dalai2@users.noreply.github.com>
2019-10-28 22:36:51 -0600

    añadí creacion de posts

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 class post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField()
@@ -11,4 +11,6 @@ class post(models.Model):
 
 	def __str__(self):
 		return self.title
-# Create your models here.
+
+	def get_absolute_url(self):
+		return reverse('post-detail', kwargs={'pk': self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView,
+ DetailView,
+  CreateView,
+  UpdateView,
+  DeleteView,)
 from .models import post
 
 def home(request):
@@ -15,6 +20,41 @@ class PostListView(ListView):
 	template_name = 'home.html'
 	context_object_name = 'posts'
 	ordering = ['-date_posted']	
+
+class PostDetailView(DetailView):
+	model = post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+	model = post
+	fields = ['title', 'content']
+
+	def form_valid(self,form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+	model = post
+	fields = ['title', 'content']
+
+	def form_valid(self,form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True	
+		return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = post
+	success_url = '/'
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True	
+		return False
 
 def about(request):
 	return render(request, 'about.html', {'title': 'About'})
