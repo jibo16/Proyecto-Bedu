9e9488e9766c43f85680475f415fbc6f6a49ced6
Dalai Aguirre <49468436+dalai2@users.noreply.github.com>
2019-10-28 19:35:30 -0600

    Cambios en desarrollo

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.urls import path
+from .views import PostListView
 from . import views
 
 urlpatterns = [
-	path('', views.home, name='blog-home'), # path para pagina principal
+	path('', PostListView.as_view(), name='blog-home'), # path para pagina principal
 	path('about/', views.about, name='blog-about'), # path para pagina de informacion
 	]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render 
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import post
 
 def home(request):
 	context = {
-		'posts': post.objects.all()
+		'posts': Post.objects.all()
 	}
 	return render(request, 'home.html', context)
+
+
+class PostListView(ListView):
+	model = post
+	template_name = 'home.html'
+	context_object_name = 'posts'
+	ordering = ['-date_posted']	
 
 def about(request):
 	return render(request, 'about.html', {'title': 'About'})
