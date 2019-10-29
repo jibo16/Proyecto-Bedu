7512ff2671984cab47b5f231d68e16ff4c4bbaa3
Dalai Aguirre <49468436+dalai2@users.noreply.github.com>
2019-10-29 00:28:49 -0600

    añadido paginacion en pagina principal y para usuarios

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (ListView,
  DetailView,
   CreateView,
@@ -19,7 +20,18 @@ class PostListView(ListView):
 	model = post
 	template_name = 'home.html'
 	context_object_name = 'posts'
-	ordering = ['-date_posted']	
+	ordering = ['-date_posted']
+	paginate_by = 4	
+
+class UserPostListView(ListView):
+	model = post
+	template_name = 'user_posts.html'
+	context_object_name = 'posts'
+	paginate_by = 4	
+
+	def get_queryset(self):
+		user = get_object_or_404(User, username=self.kwargs.get('username'))
+		return post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
 	model = post
